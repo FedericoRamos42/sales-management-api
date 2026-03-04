6a2995d5988325782d7adcbd9e049c0d77e0120e
Federico Ramos <federicojramos42@gmail.com>
2026-03-04 11:27:44 -0300

    feat: calendar event manual mapper

---
+++ src/Application/Services/Calendar/Mappers/CalendarEventMapper.cs
@@ -0,0 +1,26 @@
+﻿using Application.Services.Calendar.Models;
+using Domain.Enitites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services.Calendar.Mappers
+{
+    public static class CalendarEventMapper
+    {
+        public static CalendarEventDto ToDto(this CalendarEvent calendarEvent) => 
+            new CalendarEventDto(
+                calendarEvent.Id,
+                calendarEvent.AdminId, 
+                calendarEvent.Title,
+                calendarEvent.Description,
+                calendarEvent.StartDate.ToString(),
+                calendarEvent.EndDate.ToString(),
+                calendarEvent.Type.ToString()
+            );
+
+        public static List<CalendarEventDto> ToListDto(this List<CalendarEvent> calendarEvents) => calendarEvents.Select(e => e.ToDto()).ToList();
+    }
+}
---
+++ src/Application/Services/Calendar/Models/CalendarEventDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services.Calendar.Models
+{
+   public record CalendarEventDto(
+       int Id,
+       int UserId,
+       string Title,
+       string? Description,
+       string StartDate,
+       string? EndDate,
+       string Type
+       );
+}
