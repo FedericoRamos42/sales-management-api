cb1dffd1f3326c76621d58db5e93de73e14a8df6
Federico Ramos <federicojramos42@gmail.com>
2026-03-04 10:54:26 -0300

    feat: calendar events repository

---
+++ src/Infrastructure/Repositories/CalendarEventRepository.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Enitites;
+using Domain.Interfaces;
+using Infrastructure.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories
+{
+    public class CalendarEventRepository : BaseRepository<CalendarEvent>, ICalendarEventRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public CalendarEventRepository(ApplicationDbContext context) : base(context)
+        {
+            _context = context;
+        }
+    }
+}
